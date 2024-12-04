65c68832ee73bfdf5f89f943c20d3d6a3a55b6d1
Shaun Verch <shaun@shaunverch.com>
2024-12-03 22:55:49 -0500

    Support values in tags

--- src/types/tag.rs
+++ src/types/tag.rs
@@ -1,16 +1,18 @@
+use super::Value;
+
 /// Key-value pair that can be used to identify and organise resources.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Tag {
     #[serde(rename = "Key")]
     key: String,
     #[serde(rename = "Value")]
-    value: String
+    value: Value<String>
 }
 
 impl Tag {
     /// Create a tag from a key-value pair .
-    pub fn new<K: Into<String>, V: Into<String>>(&mut self, key: K, value: V) -> Tag {
-        Tag {
+    pub fn new<K: Into<String>, V: Into<Value<String>>>(key: K, value: V) -> Self {
+        Self {
             key: key.into(),
             value: value.into()
         }
